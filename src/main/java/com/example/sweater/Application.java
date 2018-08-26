f61efd1f808e5424298287854d91df363b90288e
syntheticfeelings <antnmslv@gmail.com>
2018-08-27 00:58:39 +0300

    upd

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -1,13 +1,10 @@
-package springBootTest;
-
+package com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
